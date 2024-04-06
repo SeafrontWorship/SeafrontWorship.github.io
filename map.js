8d1d04475beafa565d2d1a8ea3c39264d05630cd
ansonChui <chuichitoanson@gmail.com>
2024-04-06 16:06:51 +0800

    fix music page zooming in middle

--- map.js
+++ map.js
@@ -4,43 +4,43 @@ const SCALE_MAX = 10
 const SCALE_MIN = 0.1
 const SCALE_FACTOR = 0.1
 const SCALE_DEFAULT = 1
-const CANVAS_W  = 1024
-const CANVAS_H = 576
+    // const CANVAS_W = 1024
+    // const CANVAS_H = 576
 const BG_INIT_POSITION_X = 0
 const BG_INIT_POSITION_Y = 0
 
 
-// Setup
+// Setup Canvas
 const canvas = document.querySelector("canvas");
 const c = canvas.getContext('2d')
 
 // get nav bar height
-const nav = document.querySelector("#normal-nav")
+const nav = document.querySelector("#music-nav")
 const NAV_HEIGHT = nav.clientHeight
 
 //   // Set display size (vw/vh).
 var sizeWidth = window.innerWidth
 var sizeHeight = window.innerHeight - NAV_HEIGHT
-// console.log(sizeWidth, window.innerHeight, nav.clientHeight)
+    // console.log(sizeWidth, window.innerHeight, nav.clientHeight)
 canvas.width = sizeWidth;
-canvas.height = sizeHeight;
-//   canvas.style.width = sizeWidth;
-//   canvas.style.height = sizeHeight;
-console.log(window.innerWidth, window.innerHeight)
+canvas.height = sizeHeight - 10;
+canvas.style.width = sizeWidth;
+canvas.style.height = sizeHeight - 10;
+console.log(window.innerWidth, window.innerHeight, NAV_HEIGHT, canvas.width, canvas.height)
 
 
 //class definitions
 class Mouse {
-    constructor(){
+    constructor() {
         this.pressed = false;
-        this.pres_pos =  {
-            x: 0,
-            y: 0
-        }// defulat values
-        this.curr_pos =  {
-            x: 0,
-            y: 0
-        }// defulat values
+        this.pres_pos = {
+                x: 0,
+                y: 0
+            } // defulat values
+        this.curr_pos = {
+                x: 0,
+                y: 0
+            } // defulat values
         this.scrolling = false
         this.zoomIn = false
         this.zoomOut = false
@@ -48,7 +48,7 @@ class Mouse {
 }
 
 class Sprite {
-    constructor({position, image_path, scale}){
+    constructor({ position, image_path, scale }) {
         this.position = position
         this.image = new Image()
         this.image.src = image_path
@@ -57,10 +57,10 @@ class Sprite {
         console.log(this.image.width, this.image.height)
     }
 
-    draw(){
-        c.drawImage(this.image, this.position.x, this.position.y, this.image.width*this.scale, this.image.height*this.scale)
+    draw() {
+        c.drawImage(this.image, this.position.x, this.position.y, this.image.width * this.scale, this.image.height * this.scale)
     }
-    
+
     // pass in: wold coor object {x, y} (float)
     // turn: screen coor object {x, y } (iont)
     // world2screen (world_position) {
@@ -68,20 +68,20 @@ class Sprite {
     //         x: null,
     //         y: null
     //     }
-        
+
     //     screen_position.x = Math.round((world_position.x + this.position.x) * this.scale);
     //     screen_position.y = Math.round((world_position.y + this.position.y) * this.scale);
     //     return screen_position;
     // }
-    
-    screen2world (screen_position) {
+
+    screen2world(screen_position) {
         var world_position = {
             x: null,
             y: null
         }
 
-        world_position.x = Math.round(screen_position.x/this.scale - this.position.x)
-        world_position.y = Math.round(screen_position.y/this.scale - this.position.y)
+        world_position.x = Math.round((screen_position.x - this.position.x) / this.scale)
+        world_position.y = Math.round((screen_position.y - this.position.y) / this.scale)
         return world_position;
     }
 }
@@ -93,47 +93,48 @@ const bg = new Sprite({
         y: BG_INIT_POSITION_Y
     },
     image_path: "./img/demo_map.jpg",
-    scale: 1
+    scale: SCALE_DEFAULT
 })
 
 const mouse = new Mouse()
 
 // function definition
-function clean(){
-    c.clearRect(0,0,canvas.width, canvas.height)
+function clean() {
+    c.clearRect(0, 0, canvas.width, canvas.height)
 }
 
 // main function loop
-function animate(){
+function animate() {
     window.requestAnimationFrame(animate)
 
     // panning
-    if (mouse.pressed){
+    if (mouse.pressed) {
         // update bg position
         bg.position.x += mouse.curr_pos.x - mouse.pres_pos.x
         bg.position.y += mouse.curr_pos.y - mouse.pres_pos.y
-        // reset pres_pos to cur_pos
+            // reset pres_pos to cur_pos
         mouse.pres_pos = mouse.curr_pos
     }
 
     // zooming
-    // bg.scale = mouse.scale
-    if (mouse.scrolling){
+    if (mouse.scrolling) {
         var mouse_beforeZoom = bg.screen2world(mouse.curr_pos)
-    
+
         if (mouse.zoomIn) {
             bg.scale *= (1 + SCALE_FACTOR)
             mouse.zoomIn = false
-        }else if (mouse.zoomOut){
+        } else if (mouse.zoomOut) {
             bg.scale *= (1 - SCALE_FACTOR)
             mouse.zoomOut = false
         }
-        
-        var mouse_afterZoom = bg.screen2world(mouse.curr_pos)
-        console.log(mouse_beforeZoom, mouse_afterZoom)
+        // set scale in max and min!
+        bg.scale = Math.max(Math.min(bg.scale, SCALE_MAX), SCALE_MIN)
 
-        bg.position.x -= (mouse_beforeZoom.x - mouse_afterZoom.x);
-		bg.position.y -= (mouse_beforeZoom.y - mouse_afterZoom.y);
+        var mouse_afterZoom = bg.screen2world(mouse.curr_pos)
+            // console.log("mouse before and after", mouse_beforeZoom, mouse_afterZoom)
+
+        bg.position.x += (mouse_afterZoom.x - mouse_beforeZoom.x) * bg.scale;
+        bg.position.y += (mouse_afterZoom.y - mouse_beforeZoom.y) * bg.scale;
 
         mouse.scrolling = false
     }
@@ -150,37 +151,32 @@ window.addEventListener('mousedown', (e) => {
         y: e.clientY - NAV_HEIGHT
     }
     mouse.curr_pos = mouse.pres_pos
-    console.log(mouse.curr_pos, bg.screen2world(mouse.curr_pos))
+    console.log("screen vs world", mouse.curr_pos, bg.screen2world(mouse.curr_pos))
+    console.log("background", bg.position, bg.scale)
 })
 
 window.addEventListener('mouseup', (e) => {
     mouse.pressed = false
-    console.log(mouse.pressed)
+        // console.log(mouse.pressed)
 })
 
-onmousemove = function(e){
-    if(mouse.pressed){
-        mouse.curr_pos = {
-            x: e.clientX,
-            y: e.clientY - NAV_HEIGHT
-        }
+onmousemove = function(e) {
+    mouse.curr_pos = {
+        x: e.clientX,
+        y: e.clientY - NAV_HEIGHT
     }
 }
 
 window.addEventListener("wheel", (e) => {
-    console.log(e)
-    // mouse.scale += (-e.deltaY / 102) * SCALE_STEP
-    // mouse.scale = Math.min(Math.max(SCALE_MIN, mouse.scale), SCALE_MAX);
-    // console.log(mouse.scale)
     mouse.scrolling = true
     mouse.curr_pos = {
         x: e.clientX,
         y: e.clientY - NAV_HEIGHT
     }
-    if (e.deltaY > 0){
+    if (e.deltaY > 0) {
         mouse.zoomOut = true
         console.log("ZoomOut")
-    }else{
+    } else {
         mouse.zoomIn = true
         console.log("ZoomIn")
     }
@@ -188,4 +184,3 @@ window.addEventListener("wheel", (e) => {
 
 //start animate loop
 animate()
-
